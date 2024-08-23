c8b1364408d52107faea219396cc5b5f318aa645
Amestest <gktk8411@naver.com>
2024-08-23 12:06:17 +0900

    search

--- JPA_chicken/src/main/java/com/kh/controller/ChickenController.java
+++ JPA_chicken/src/main/java/com/kh/controller/ChickenController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.dto.Chicken;
@@ -49,5 +50,11 @@ public class ChickenController {
 	@DeleteMapping("{id}")
 	public void dlelteChicken(@PathVariable("id") Integer id){
 		chickenService.deleteChicken(id);
+	}
+	
+	//검색 기능 Get
+	@GetMapping("/search")
+	public List<Chicken> searchChickens(@RequestParam("query") String query){
+		return chickenService.searchChickens(query);
 	}
 }
--- chicken-store/src/App.js
+++ chicken-store/src/App.js
@@ -10,6 +10,7 @@ import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import ChickenList from "./component/ChickenList";
 import ChickenDetail from "./component/ChickenDetail";
 import MainRouter from "./MainRouter";
+import SearchResult from "./component/SearchResult";
 
 function App () {
     return(
@@ -18,6 +19,7 @@ function App () {
                 <Route path='/' element={<MainRouter/>}/>
                 {/* Routes 안에는 Route로 설정된 태그만 들어올 수 있음  <MainRouter/>*/}
                 <Route path='/chicken-detail/:id' element={<ChickenDetail/>}/>
+                <Route path="/search" element={<SearchResult/>}/>
             </Routes>
         </Router>
     )
